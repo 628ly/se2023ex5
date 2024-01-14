6c087b98a64d2708e3a83e1a8162daaa2a1167a8
628ly <r202203496pq@jindai.jp>
2024-01-15 02:40:48 +0900

    for loop

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
 #include<stdio.h>{
 
     int main(void){
+        
         int i = 5;
 
-        printf("Hello Wprld!, %d\n", &i);
+        for(i=0; i<10; i++){
+
+            printf("Hello Wprld!, %d\n", &i);
+
+        }
 
         return 0;
     }
